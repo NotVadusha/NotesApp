5072cf3283914131e59797bfd5191d2362f8b80c
NotVadusha <vadimkbondarchuk@gmail.com>
2023-08-05 20:41:09 +0300

    feat: import database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgress",
+      password: "root",
+      database: "notes",
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
