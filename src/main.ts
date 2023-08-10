4fcf2245a81417ba7a1471a0c244beec6023c74b
NotVadusha <vadimkbondarchuk@gmail.com>
2023-08-10 17:38:54 +0300

    feat: add global validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import "reflect-metadata";
+import { ValidationPipe } from "@nestjs/common";
 
 async function start() {
   const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(PORT, () => {
     console.log(`Listening ${PORT} port`);
   });
